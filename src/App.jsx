dd0f417bfd75bcfb3b178571acf71da3be3b0c6b
PhilippeCLeger <philippe-cosmo.leger@outlook.com>
2023-04-05 13:50:18 -0400

    Added tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {Box, Button, TextField} from '@mui/material'
+import {Box, Button, TextField, Tab} from '@mui/material'
+import {TabContext, TabList, TabPanel} from '@mui/lab'
 import OuvragesList from './Components/OuvragesList'
 import ExampleForm from './Components/ExampleForm'
 import Accordion from '@mui/material/Accordion';
@@ -6,11 +7,27 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import { Card, Grid, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+import { TYPES_OUVRAGES } from './Enums'
+import {useState} from 'react'
+
 function App() {
+  const [type, setType] = useState(TYPES_OUVRAGES.LIVRE.toString());
   return (
     <div className="App">
-      <OuvragesList />
-      <ExampleForm />
+      <TabContext value={type}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={(e, val) => setType(val.toString())} aria-label="lab API tabs example">
+            <Tab label="Livres" value={TYPES_OUVRAGES.LIVRE.toString()} />
+            <Tab label="Périodiques"  value={TYPES_OUVRAGES.PÉRIODIQUE.toString()} />
+          </TabList>
+        </Box>
+        {Object.values(TYPES_OUVRAGES).map(type => (
+          <TabPanel value={type.toString()}>
+            <OuvragesList type={type}/>
+            <ExampleForm/>
+          </TabPanel>
+        ))}
+      </TabContext>
     </div>
   )
 }
--- src/Components/OuvragesList.jsx
+++ src/Components/OuvragesList.jsx
@@ -5,8 +5,8 @@ import { getOuvrages } from "../API";
 import useGetOuvrages from '../hooks/useGetOuvrages';
 import OuvrageListItem from "./OuvrageListItem";
 
-const OuvragesList = () => {
-    const {ouvrages, refreshOuvrages} = useGetOuvrages();
+const OuvragesList = ({type}) => {
+    const {ouvrages, refreshOuvrages} = useGetOuvrages(type);
     return <Box sx={{display:'flex', flexDirection:'column', gap:1}}>
         <Button onClick={refreshOuvrages}>REFRESH</Button>
         {ouvrages && 
