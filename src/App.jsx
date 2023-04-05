724c0cfe25c2c71d30fc69ca051f9d49d7627255
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2023-04-05 13:42:20 -0400

    Added Accordion for List Item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import {Box, Button, TextField} from '@mui/material'
 import OuvragesList from './Components/OuvragesList'
 import ExampleForm from './Components/ExampleForm'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import { Card, Grid, Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 function App() {
   return (
     <div className="App">
--- src/Components/OuvrageListItem.jsx
+++ src/Components/OuvrageListItem.jsx
@@ -1,23 +1,41 @@
 import { Box, Card, Grid, Typography } from "@mui/material";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { useState } from "react";
+
 
 const OuvrageListItem = ({ouvrage}) => {
     const estDisponible = ouvrage.exemplaires.findIndex(ex => ex.estDisponible) >= 0;
-    return <Box>
-        <Card sx={{
+    return <Accordion sx={{
             borderLeft:8,  
             borderColor: estDisponible ? "success.main" : "error.main", 
             p:"0.5rem",
             }}>
-            <Grid container direction={"column"}>
-                <Grid item>
-                    <Typography sx={{fontWeight:"bold", fontSize:"1.25rem", color:"primary.main"}}>
-                        {ouvrage.titre}
-                    </Typography>
-                </Grid>
-            </Grid>
-
-        </Card>
-    </Box>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id={ouvrage.id}
+            >
+            <Typography sx={{fontWeight:"bold", fontSize:"1.25rem", color:"primary.main"}}>
+                {ouvrage.titre}
+            </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box>
+            <Box>Année d'édition: {ouvrage.annee}</Box>
+            <Box>Maison d'édition: {ouvrage.maison}</Box>
+            <Box>Auteur: {ouvrage.auteur}</Box>
+            {ouvrage.exemplaires.map((ex => 
+                <Box key={ex.no} sx={{display:"flex", gap: 1}}>
+                    <Box sx={{borderRadius: "50%", width: "1rem", height: "1rem", backgroundColor: ex.estDisponible ? "success.main" : "error.main"}}/>
+                    <Box key="no">No: {ex.no}</Box>
+                </Box>))}
+          </Box>
+        </AccordionDetails>
+    </Accordion>
 }
 
 export default OuvrageListItem;
